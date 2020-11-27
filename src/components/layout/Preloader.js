74f80fcaaa039f4834c3ece8f1b6db91a679050a
Rohan <rohanshakya254@gmail.com>
2020-11-27 18:47:09 +0545

    completed app:v1

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Preloader = () => {
+  return (
+    <div className='progress blue lighten-blue'>
+      <div className='indeterminate blue'></div>
+    </div>
+  );
+};
